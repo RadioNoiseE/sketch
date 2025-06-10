02ed5619b434e50d766eb6b500bdb86353278c53
RadioNoiseE <j18516785606@icloud.com>
2025-06-10 09:37:14 +0800

    Resize and insert logic, with fixup when freeing

--- llp/hashtable.c
+++ llp/hashtable.c
@@ -1,4 +1,5 @@
 #include "hashtable.h"
+#include "prime.h"
 
 static unsigned int map (const char *string, size_t size) {
   unsigned int hash = 0;
@@ -20,9 +21,32 @@ static void delete (bucket *bucket) {
   free (bucket);
 }
 
+static void resize (hashtable *target, size_t size) {
+  hashtable *temp = malloc (sizeof (hashtable));
+  temp->size      = size;
+  temp->count     = 0;
+  temp->buckets   = calloc (temp->size, sizeof (bucket *));
+
+  for (size_t i = 0; i < target->size; i++) {
+    bucket *bucket = target->buckets[i];
+    if (bucket != NULL)
+      hashtable_insert (temp, bucket->key, bucket->value);
+  }
+
+  size_t temp_size = target->size;
+  target->size     = temp->size;
+  temp->size       = temp_size;
+
+  bucket **temp_buckets = target->buckets;
+  target->buckets       = temp->buckets;
+  temp->buckets         = temp_buckets;
+
+  hashtable_destory (temp);
+}
+
 hashtable *hashtable_new (void) {
   hashtable *this = malloc (sizeof (hashtable));
-  this->size      = 53; // fix: prime sequence
+  this->size      = 53;
   this->count     = 0;
   this->buckets   = calloc (this->size, sizeof (bucket *));
   return this;
@@ -32,10 +56,15 @@ void hashtable_insert (hashtable *hashtable, const char *key,
                        const char *value) {
   bucket *current = new (key, value);
   size_t  offset  = map (current->key, hashtable->size);
-  while (hashtable->buckets[offset] != NULL) {
-    offset++;
-    // TODO: handle resize
-  }
+
+  if ((float) hashtable->count / hashtable->size > 0.7)
+    resize (hashtable, prime (hashtable->size * 2));
+
+  while (hashtable->buckets[offset] != NULL)
+    offset = offset + 1 % hashtable->size;
+
+  hashtable->buckets[offset] = current;
+  hashtable->count++;
 }
 
 void hashtable_delete (hashtable *hashtable, const char *key) {}
@@ -47,7 +76,8 @@ void hashtable_destory (hashtable *hashtable) {
     bucket *current = hashtable->buckets[i];
     if (current != NULL)
       delete (current);
-    free (hashtable->buckets);
-    free (hashtable);
   }
+
+  free (hashtable->buckets);
+  free (hashtable);
 }
